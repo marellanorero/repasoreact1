75594567654cf6cd3197ade2fae462452361af38
marellanorero <marellanonorero@gmail.com>
2022-05-31 21:39:32 -0400

    commit

---
+++ todolist-react/todolist-react/src/components/Checkbox.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+const Checkbox = props => {
+    const {
+        onChange,
+        data: {id, description, done}
+    } = props;
+    return(
+        <Fragment>
+            <label className='todo new-item'>
+                <input className='todo__state' name={id} type="checkbox" defaultChecked={done} onChange={onChange}>
+                </input>
+                <div className='todo__text'>{description}</div>
+            </label>
+        </Fragment>
+    )
+}
+
+export default Checkbox
